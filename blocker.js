5bfb2bf8c2d5e5dd03a19e40002fb71e0938656c
Danucosuko suko <71569318+Danucosukosuko@users.noreply.github.com>
2023-05-13 23:08:05 +0200

    Update blocker.js

--- blocker.js
+++ blocker.js
@@ -4,13 +4,13 @@
 // @version      2023.05.13
 // @description  Bloquear el acceso a una página web y mostrar un mensaje de acceso denegado. es libre de ser modificada. (https://github.com/Danucosukosuko)
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
+// @updateURL    https://raw.githubusercontent.com/Danucosukosuko/WebBlocker/main/blocker.js
+// @downloadURL  https://raw.githubusercontent.com/Danucosukosuko/WebBlocker/main/blocker.js
 // @require      https://raw.githubusercontent.com/Danucosukosuko/WebBlocker/main/blocker.js
 // @match        http://127.0.0.1:8881/*
 // @match        https://malavida.com/*
 // @match        https://softonic.com/*
 // @match        https://google.com/*
-// @match        https://chat.openai.com/*
-// @match        https://*
 // @match        about:blank/*
 // @grant        GM_setValue
 // @grant        GM_getValue
@@ -25,7 +25,7 @@
     const body = document.body;
 
     // Esta siguiente línea es el contenido de la web una vez borrada.
-    const webcontent = "<h1>No se puede acceder al sitio web. Posibles causas: El sitio web se ha considerado inseguro o tiene virus.</h1><h1>Te recomendamos que vuelvas a tu página anterior</h1><h1>Si quiere contribuir a alguna web, vete a esta URL: https://todavianohayweb.com</h1><img src='https://w7.pngwing.com/pngs/571/181/png-transparent-warning-sign-safety-alerte-angle-triangle-warning-sign.png' style='width: 512px; height: 256px;'>";
+    const webcontent = "<h1>No se puede acceder al sitio web. Posibles causas: El sitio web se ha considerado inseguro o tiene virus.</h1><h1>Te recomendamos que vuelvas a tu página anterior</h1><h1>Si quiere contribuir a alguna web, vete a esta URL: https://github.com/Danucosukosuko/WebBlocker</h1><img src='https://w7.pngwing.com/pngs/571/181/png-transparent-warning-sign-safety-alerte-angle-triangle-warning-sign.png' style='width: 512px; height: 256px;'>";
 
     // Guardar el contenido original de la web en una cookie
     document.cookie = "webcontent=" + encodeURIComponent(body.innerHTML) + "; path=/";
